2c380746d8805f93c9003bf5360e51799b25e7f8
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-03-13 14:29:10 +0700

    สร้างไว้เรียนการทำงานของ State

---
+++ CounterView.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CounterView = () => {
+  return (
+    <View>
+      <Text>CounterView</Text>
+    </View>
+  )
+}
+
+export default CounterView
